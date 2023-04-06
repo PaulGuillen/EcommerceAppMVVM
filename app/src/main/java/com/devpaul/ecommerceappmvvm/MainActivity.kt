87223d337191fe211d1af9b7d26395c10b23fc40
Paul Guillen <paulguillen190@gmail.com>
2023-04-05 19:58:14 -0600

    Diseño de la pantalla principal parte uno

--- app/src/main/java/com/devpaul/ecommerceappmvvm/MainActivity.kt
+++ app/src/main/java/com/devpaul/ecommerceappmvvm/MainActivity.kt
@@ -3,13 +3,29 @@ package com.devpaul.ecommerceappmvvm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.devpaul.ecommerceappmvvm.ui.theme.EcommerceAppMVVMTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +34,48 @@ class MainActivity : ComponentActivity() {
         setContent {
             EcommerceAppMVVMTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LoginContent()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun LoginContent() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Imagen de Fondo",
+            contentScale = ContentScale.Crop,
+            colorFilter = ColorFilter.colorMatrix(
+                ColorMatrix().apply {
+                setToScale(0.5f,0.5f,0.5f,1f)
+            })
+        )
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(top = 50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(100.dp),
+                painter = painterResource(id = R.drawable.shopping_cart_blue),
+                contentDescription = "Logo"
+            )
+            Text(
+                modifier = Modifier.padding(top = 7.dp),
+                text = "ECOMMERCE APP",
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                fontSize = 20.sp
+            )
         }
     }
 }
@@ -32,10 +85,10 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     EcommerceAppMVVMTheme {
-        Greeting("")
+        LoginContent()
     }
 }
